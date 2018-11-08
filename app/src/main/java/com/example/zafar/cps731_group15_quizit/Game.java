22da708d3c2dbce48486d0d4838a6dcf409f7a5f
Zafar Ali <zafar.a.ali2@gmail.com>
2018-11-07 20:38:00 -0500

    Log out option
    Still no SQL implementation

--- app/src/main/java/com/example/zafar/cps731_group15_quizit/Game.java
+++ app/src/main/java/com/example/zafar/cps731_group15_quizit/Game.java
@@ -1,16 +1,19 @@
 package com.example.zafar.cps731_group15_quizit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private Button logout;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -37,8 +40,23 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game3);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        logout = (Button)findViewById(R.id.logout);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Game.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
     }
 
-}
+
+
+
