dc756210d4c9aebd37a1b4a7fee269447f9d80ac
Zafar Ali <zafar.a.ali2@gmail.com>
2018-11-14 18:31:41 -0500

    User can select category and timer
    Uses dummy array as SQL is not yet implemented

--- app/src/main/java/com/example/zafar/cps731_group15_quizit/Play.java
+++ app/src/main/java/com/example/zafar/cps731_group15_quizit/Play.java
@@ -1,5 +1,6 @@
 package com.example.zafar.cps731_group15_quizit;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -9,16 +10,20 @@ import android.widget.TextView;
 public class Play extends AppCompatActivity {
     //QuizOptions qz= new QuizOptions();
     private TextView timer;
-
-
+    private TextView category;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        timer=(TextView)findViewById(R.id.timerTrue);
+        timer=(TextView)findViewById(R.id.timeTV);
+        category=(TextView)findViewById(R.id.categoryTV);
         Boolean timerChoice= getIntent().getExtras().getBoolean("timerChoice");
+        String categoryChoice= getIntent().getExtras().getString("category");
+        category.setText(categoryChoice);
+
+        
         if (timerChoice) {
             timer.setText("Timer is true");
         }else{
--- app/src/main/java/com/example/zafar/cps731_group15_quizit/QuizOptions.java
+++ app/src/main/java/com/example/zafar/cps731_group15_quizit/QuizOptions.java
@@ -1,9 +1,11 @@
 package com.example.zafar.cps731_group15_quizit;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,23 +14,88 @@ import android.widget.Spinner;
 
 public class QuizOptions extends AppCompatActivity {
     private String[] dummyArray = {"Math", "Science", "Geography", "Movies", "MISC"};
-    public boolean timerChoice = false;
+    private boolean timerChoice = false;
     private Button logoutbtn;
     private Button playbtn;
-    public String categoryChoice = "";
+    private String categoryChoice = "";
     private CheckBox timer;
+    Spinner dropdown;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_options);
-        Spinner dropdown = findViewById(R.id.spinner1);
+
         timer = (CheckBox) findViewById(R.id.timerOption);
+
+        dropdown = findViewById(R.id.categorySpinner);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, dummyArray);
         dropdown.setAdapter(adapter);
-        categoryChoice = dropdown.getSelectedItem().toString();
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                //use postion value
+
+                switch (position)
+
+                {
+
+                    case 0:
+
+                        categoryChoice=dummyArray[0];
+
+                        break;
+
+                    case 1:
+
+                        categoryChoice = dummyArray[1];
+
+                        break;
+
+                    case 2:
+
+                        categoryChoice = dummyArray[2];
+
+                        break;
+
+                    case 3:
+
+                        categoryChoice = dummyArray[3];
+
+                        break;
+
+                    case 4:
+
+                        categoryChoice = dummyArray[4];
+
+                        break;
+
+
+
+                }
+
+            }
+
+            @Override
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+
+
+
 
 
         logoutbtn = (Button) findViewById(R.id.logoutbtn);
@@ -49,11 +116,13 @@ public class QuizOptions extends AppCompatActivity {
                 if (timerChoice) {
                     Intent intent = new Intent(QuizOptions.this, Play.class);
                     intent.putExtra("timerChoice", true);
+                    intent.putExtra("category",categoryChoice);
 
                     startActivity(intent);
                 }else{
                     Intent intent = new Intent(QuizOptions.this, Play.class);
                     intent.putExtra("timerChoice", false);
+                    intent.putExtra("category",categoryChoice);
                     startActivity(intent);
                 }
 
