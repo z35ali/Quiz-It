77578696633e2aa738cf247f73de15c32c48503f
Zafar Ali <zafar.a.ali2@gmail.com>
2018-11-14 13:13:08 -0500

    Layout for Game Screen

--- app/src/main/java/com/example/zafar/cps731_group15_quizit/Game.java
+++ app/src/main/java/com/example/zafar/cps731_group15_quizit/Game.java
@@ -1,62 +1,66 @@
 package com.example.zafar.cps731_group15_quizit;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private Button logout;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private Button playbtn;
+    private Button cqbtn;
+    private Button reportbtn;
+    private Button purchasebtn;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText("Game");
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText("Custom Quizzes");
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText("Store and Report Bugs");
-                    return true;
-            }
-            return false;
-        }
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game3);
+        setContentView(R.layout.activity_game);
+        playbtn=(Button)findViewById(R.id.playbtn);
+        cqbtn=(Button)findViewById(R.id.cqbtn);
+        reportbtn=(Button)findViewById(R.id.reportbtn);
+        purchasebtn = (Button)findViewById(R.id.purchasebtn);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        logout = (Button)findViewById(R.id.logout);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        playbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(Game.this, Login.class);
+                Intent intent= new Intent(Game.this, Play.class);
                 startActivity(intent);
 
             }
         });
 
+        purchasebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Game.this, Hints.class);
+                startActivity(intent);
+
+            }
+        });
+        reportbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Game.this, Report.class);
+                startActivity(intent);
+
+            }
+        });
+        cqbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Game.this, CustomQuiz.class);
+                startActivity(intent);
+
+            }
+        });
     }
-    }
 
-
-
-
+}
---
+++ app/src/main/java/com/example/zafar/cps731_group15_quizit/Hints.java
@@ -0,0 +1,13 @@
+package com.example.zafar.cps731_group15_quizit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hints);
+    }
+}
