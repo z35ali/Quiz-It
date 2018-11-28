b7b8cbfb46c36645b9f2151cbfc78740f4ee2bc2
Zafar Ali <zafar.a.ali2@gmail.com>
2018-11-28 12:53:10 -0500

    Question deprecated 1

--- app/src/main/java/com/example/zafar/cps731_group15_quizit/Quiz.java
+++ app/src/main/java/com/example/zafar/cps731_group15_quizit/Quiz.java
@@ -3,21 +3,30 @@ package com.example.zafar.cps731_group15_quizit;
 import java.util.ArrayList;
 
 public class Quiz {
-    public ArrayList<Question> questions;
+    public ArrayList<String> questions;
+    public ArrayList<String> answers;
+    public ArrayList<String> hints;
+    
     public String name;
     public String author;
     public String category;
+    public String difficulty;
 
-    public Quiz (String name, String author, String category) {
+    public Quiz (String name, String author, String category, String difficulty) {
 
         this.name = name;
         this.author = author;
         this.category = category;
+        this.difficulty = difficulty;
 
         questions = new ArrayList<>();
+        answers = new ArrayList<>();
+        hints = new ArrayList<>();
     }
 
-    public void addQuestion(String question_text, String hint_text, String[] answer_texts, int correct_index) {
-        questions.add(new Question(question_text, hint_text, answer_texts, correct_index));
+    public void addQuestion(String question_text, String hint_text, String answer_text) {
+        questions.add(question_text);
+        hints.add(hint_text);
+        answers.add(answer_text);
     }
 }
