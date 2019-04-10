38a7af3ea4c282c12b5bd852de2281475b4f34f4
Zafar <zafar.a.ali2@gmail.com>
2019-04-09 23:38:56 -0400

    Color Changes
    - Blue Title and Status Bar
    - Grey Background (Off-white)

--- app/src/main/java/com/zafar/quizit/QuizActivity.java
+++ app/src/main/java/com/zafar/quizit/QuizActivity.java
@@ -231,15 +231,15 @@ public class QuizActivity extends AppCompatActivity {
 
         switch(currentQuestion.getAnswerNr()){
             case 1:
-                rb1.setTextColor(Color.GREEN);
+                rb1.setTextColor(Color.parseColor("#1177c9"));
                 textViewQuestion.setText("Answer 1 is correct");
                 break;
             case 2:
-                rb2.setTextColor(Color.GREEN);
+                rb2.setTextColor(Color.parseColor("#1177c9"));
                 textViewQuestion.setText("Answer 2 is correct");
                 break;
             case 3:
-                rb3.setTextColor(Color.GREEN);
+                rb3.setTextColor(Color.parseColor("#1177c9"));
                 textViewQuestion.setText("Answer 3 is correct");
                 break;
 
